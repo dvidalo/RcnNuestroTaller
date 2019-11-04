927020e95ef82e9c882a7ef93a757f23b8d0f39f
varr1904 <56983924+varr1904@users.noreply.github.com>
2019-11-03 22:38:17 -0500

    Add files via upload

---
+++ RcnNuestroTaller/RcnNuestroTaller/src/RCN/Iniciando.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RCN;
+
+/**
+ *
+ * @author diego vidal
+ */
+public class Iniciando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Programa object = new Programa();
+        object.setVisible(true);
+        System.out.println("iniciando");
+    }
+    
+}
