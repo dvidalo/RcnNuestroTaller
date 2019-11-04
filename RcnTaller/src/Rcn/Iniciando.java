61a9206ef1f05c0ab46340dcd992e3f537e4373e
92Gomez <45211923+92Gomez@users.noreply.github.com>
2019-11-03 19:22:15 -0500

    nueva actualizacion

---
+++ RcnTaller/src/Rcn/Iniciando.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rcn;
+
+/**
+ *
+ * @author mrdie
+ */
+public class Iniciando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Deportes object = new Deportes();
+        object.setVisible(true);
+        System.out.println("iniciando");
+    }
+    
+}
